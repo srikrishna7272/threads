c99adfffd07156083a9db004db2fd1cc4624a78f
srikrishna7272 <105886453+srikrishna7272@users.noreply.github.com>
2023-10-20 10:48:19 -0700

    Built the layout for root.

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -14,7 +14,18 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+          <main>
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSidebar />
+          </main>
+
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
